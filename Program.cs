d3f104b9c3efb2a504a90e932b1f993c26501c1a
Daniil Knaub <andy.typing@mail.ru>
2026-02-22 20:07:54 +0800

    Decomposition has been performed: the Logic class has been added

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 ﻿namespace PTFirstLabThirdTask
 {
-    internal class Program
+    class Logic
     {
-        static void Main(string[] args)
+        public static int GetExpressionResult(string inputText)
         {
-            Console.Write("Введите текст вида d_1±d_2±...±d_n, где d_i — цифры (n > 1): ");
-
-            string inputText = Console.ReadLine();
-
             int currentNumber = 0;
             char currentSign = '+';
 
             int numberMultiplier;
             int textNumbersSum = 0;
 
-            foreach (char c in inputText) 
+            foreach (char c in inputText)
             {
                 if (c == '+' || c == '-')
                 {
-                    numberMultiplier = (currentSign == '-') ? -1: 1;
+                    numberMultiplier = (currentSign == '-') ? -1 : 1;
                     textNumbersSum += numberMultiplier * currentNumber;
                     currentNumber = 0;
                     currentSign = c;
@@ -31,6 +27,20 @@
 
             numberMultiplier = (currentSign == '-') ? -1 : 1;
             textNumbersSum += numberMultiplier * currentNumber;
+
+            return textNumbersSum;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст вида d_1±d_2±...±d_n, где d_i — цифры (n > 1): ");
+
+            string inputText = Console.ReadLine();
+
+            int textNumbersSum = Logic.GetExpressionResult(inputText);
 
             Console.WriteLine($"Вычисленная сумма равна {textNumbersSum}");
         }
