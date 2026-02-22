dd4202db957baba27587289050d28da3ead5c93a
Daniil Knaub <andy.typing@mail.ru>
2026-02-22 20:56:38 +0800

    Added tests for the GetSumOfNumbersInText method

---
+++ UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using PTFirstLabThirdTask;
+
+namespace PTFirstLabThirdTask
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("1+2+3+4+5+6+7+8+9", ExpectedResult = 45)]
+        [TestCase("-1-2-3-4-5-6", ExpectedResult = -21)]
+        [TestCase("1-2-3-4-5-6", ExpectedResult = -19)]
+        [TestCase("0+0-0+0-0+0", ExpectedResult = 0)]
+        [TestCase("1-1+1-1+1-1", ExpectedResult = 0)]
+        [TestCase("-1+-+20", ExpectedResult = 19)]
+        [TestCase("23", ExpectedResult = 23)]
+        public int TestMethod_GetSumOfNumbersInText(string inputText) 
+        {
+            return Logic.GetSumOfNumbersInText(inputText);
+        }
+    }
+}
