a369b710b1c71c13b5977fb672411c871e75f1a9
Daniil Knaub <andy.typing@mail.ru>
2026-02-22 19:28:05 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PTFirstLabThirdTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
